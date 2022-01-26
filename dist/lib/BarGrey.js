aa42a1b2510768a00a53f8e580bdbbe2bf0ed4fe
SobingMa <1772907731@qq.com>
2022-01-26 09:18:24 +0800

    修改copy right

--- dist/lib/BarGrey.js
+++ dist/lib/BarGrey.js
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright 2021-present woodare_xiaolu and other contributors
+ * Copyright 2021-present xiaolu521
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
